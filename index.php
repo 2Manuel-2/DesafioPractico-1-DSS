8cee45f50aabe86dd5894c104a333b295a059860
Stevenhzw <100164210+Stevenhzw@users.noreply.github.com>
2023-03-02 20:05:15 -0600

    vista

--- index.php
+++ index.php
@@ -3,50 +3,45 @@
 <head>
     <meta charset="utf-8">
     <title>Calculadora de CUM</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/Style.css">
 </head>
 <body>
-<div class="container">
-    <h1 class="page-header text-center">Calculadora de CUM</h1>
-    <div class="row">
-        <div class="col-sm-8 col-sm-offset-2">
-            <a href="#addnew" class="btn btn-primary" data-toggle="modal"><span class="glyphicon glyphicon-plus"></span> Agregar materia</a>
-            
-            <table class="table table-bordered table-striped" style="margin-top:20px;">
-                <thead>
-                    <th>Codigo</th>
-                    <th>Nombre</th>
-                    <th>Descripción</th>
-                    <th>Imagen</th>
-                    <th>Categoria</th>
-                    <th>Preción</th>
-                    <th>existencias</th>
-                </thead>
-                <tbody>
-                   <?php
-                    $productos=simplexml_load_file("productos.xml");
-                   
-                    foreach($productos->producto as $pro){
-                      
-                   ?> 
-                   <tr>
-                    <td><?= $pro->codigo?></td>
-                    <td><?= $pro->nombre?></td>
-                    <td><?= $pro->descripcion?></td>
-                    <td><?= $pro->img?></td>
-                    <td><?= $pro->categoria?></td>
-                    <td><?= $pro->precio?></td>
-                    <td><?= $pro->existencias?></td>
-                    
-                   </tr>
-                   <?php
-                   }
-                   ?>
-                </tbody>
-            </table>
-         
+    
+    <div class="container">
+
+    <div class="title">
+            <h1>TextilExport</h1>
         </div>
-    </div>
+        <hr>
+
+        <?php
+        $productos=simplexml_load_file("productos.xml");
+        foreach($productos->producto as $pro){
+            ?> 
+            <div class="card">
+                
+            <!--<p>Descripción: <?//=$pro->descripcion?></p>-->
+                <img src="img/<?= $pro->img?>" class="cardFoto" alt="Foto">
+            <!--<p>Categoria: <?//=$pro->categoria?></p>-->
+            <div class="cardInfo">
+                    <p><?= $pro->nombre?></p>
+            <!--<p><?//= $pro->codigo?></p>-->
+                </div>
+                <p>$<?= $pro->precio?></p>
+            <!--<p>Existencias: <?//=$pro->existencias?></p>-->
+            <div class="buttonView"><a href="">Ver</a></div>
+            </div>
+            
+            <?php
+            }
+            ?>
+               
+  
+            
+            
+                
+                   
+       
 </div>
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
